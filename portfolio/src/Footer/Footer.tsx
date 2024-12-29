19c2035cd656b8a3d417365ff2916bcea603a6ff
hkattt <hugo.kat3@gmail.com>
2024-12-29 11:51:17 +1100

    Added copyright to the footer

--- portfolio/src/Footer/Footer.tsx
+++ portfolio/src/Footer/Footer.tsx
@@ -3,13 +3,16 @@ import styles from "./Footer.module.css"
 function Footer() {
     return(
         <footer className={styles.footer}>
-            <div className={styles.content}>
+            <span className={styles.content}>
                 <p className={styles.text}>
-                    ADD COPY RIGHT STUFF HERE | Designed in <a href="https://www.figma.com" target="_blank" rel="noopener noreferrer">Figma</a>, 
+                    Designed in <a href="https://www.figma.com" target="_blank" rel="noopener noreferrer">Figma</a>, 
                     built using <a href="https://react.dev/" target="_blank" rel="noopener noreferrer">ReactJS</a>, 
                     with the <a href="https://www.nordtheme.com/docs/colors-and-palettes" target="_blank" rel="noopener noreferrer">Nord theme</a>.
                 </p>
-            </div>
+                <p className={styles.copyright}>
+                    © 2025 Hugo Kat. All rights reserved.
+                </p>
+            </span>
         </footer>
     )
 }
