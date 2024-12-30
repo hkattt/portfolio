2be791d27ca5607f26acb4eb4e41ea50ed987b9e
hkattt <hugo.kat3@gmail.com>
2024-12-31 09:51:12 +1100

    Redesigned hero section

--- portfolio/src/Content/Hero/Hero.tsx
+++ portfolio/src/Content/Hero/Hero.tsx
@@ -5,8 +5,10 @@ function Hero() {
     return(
         <section id="Hero" aria-label="Hero" className={styles.hero}>
             <div className={styles.content}>
-                <h1 className={styles.title}>Hugo Kat</h1>
-                <p className={styles.introduction}>Junior software engineer and computer science student @ the ANU</p>
+                <h1 className={styles.title}>Hey! I'm Hugo Kat <br/>Software Engineer</h1>
+                <span className={styles.border}>
+                    <img className={styles.picture} src="hugo.png" alt="Profile Picture"/>
+                </span>
             </div>
         </section>
     )
