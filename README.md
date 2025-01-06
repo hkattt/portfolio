8f0b3b6032214bccd49f87b5da9d7788b055e773
hkattt <hugo.kat3@gmail.com>
2025-01-07 10:05:27 +1100

    Setting up fonts and colours

--- README.md
+++ README.md
@@ -4,7 +4,10 @@
 This is my personal website made using Vite, React, and TypeScript
 
 ## Dependencies
-TODO
+- Node
+- NextJS
+- SaSS
+- Next Fonts
 
 ## Assets
 The icons used in this website were retrieved from the following sites: [Simple Icons](https://simpleicons.org/), [SVG Repo](https://www.svgrepo.com/), and [Icon Icons](https://icon-icons.com/). Each icon has a corresponding attribution if needed. 
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -4,7 +4,13 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>Header 1</h1>
+      <h2>Header 2</h2>
+      <h3>Header 3</h3>
+      <p className={styles.large}>Large</p>
+      <p className={styles.bodyBold}>Body Bold</p>
+      <p className={styles.body}>Body</p>
+      <p className={styles.small}>Small</p>
     </main> 
   )
 }
