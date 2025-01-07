62102897a1aecca780a5be08a58afc02e3d282b2
hkattt <hugo.kat3@gmail.com>
2025-01-07 18:50:29 +1100

    Seperated Header and Footer from Main

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,11 +1,15 @@
-import Content from "./components/Content/Content";
+import Hero from "./components/Hero/Hero";
 import Footer from "./components/Footer/Footer";
+
+import styles from "./page.module.scss";
 
 export default function Main() {
   return (
-    <main>
-      <Content></Content>
+    <>
+      <main className={styles.portfolio}>
+        <Hero></Hero>
+      </main> 
       <Footer></Footer>
-    </main> 
+    </>
   )
 }
