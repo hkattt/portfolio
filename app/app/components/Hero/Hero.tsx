7dcf11cb345232002f4ab26a84f74865a9d3f420
hkattt <hugo.kat3@gmail.com>
2025-01-07 18:17:51 +1100

    Adding section tag

--- app/app/components/Hero/Hero.tsx
+++ app/app/components/Hero/Hero.tsx
@@ -6,36 +6,38 @@ import styles from "./Hero.module.scss";
 
 const Hero = () => {
   return (
-    <div className={styles.container}>
-        <div>
-            <h1>
-                Hey! I&apos;m <span>Hugo Kat </span>
-                <br />
-                Software Engineer
-            </h1>
-            <span>
-                <IconButton
-                    link="mailto:hugo.kat3@gmail.com"
-                    src="/email.svg"
-                    alt="Email Icon"
-                >
-                </IconButton>
-                <IconButton
-                    link="https://github.com/hkattt"
-                    src="/github.svg"
-                    alt="GitHub Icon"
-                >
-                </IconButton>
-                <IconButton
-                    link="https://www.linkedin.com/in/hugo-kat/"
-                    src="/linkedin.svg"
-                    alt="LinkedIn Icon"
-                >
-                </IconButton>
-            </span>
+    <section id="Hero" aria-label="Hero">
+        <div className={styles.container}>
+            <div>
+                <h1>
+                    Hey! I&apos;m <span>Hugo Kat </span>
+                    <br />
+                    Software Engineer
+                </h1>
+                <span>
+                    <IconButton
+                        link="mailto:hugo.kat3@gmail.com"
+                        src="/email.svg"
+                        alt="Email Icon"
+                    >
+                    </IconButton>
+                    <IconButton
+                        link="https://github.com/hkattt"
+                        src="/github.svg"
+                        alt="GitHub Icon"
+                    >
+                    </IconButton>
+                    <IconButton
+                        link="https://www.linkedin.com/in/hugo-kat/"
+                        src="/linkedin.svg"
+                        alt="LinkedIn Icon"
+                    >
+                    </IconButton>
+                </span>
+            </div>
+            <Image className={styles.picture} src="/hugo.png" alt="Hugo Profile Picture" width={0} height={0} sizes="100vw" style={{width: "100%", height: "auto"}}/>
         </div>
-        <Image className={styles.picture} src="/hugo.png" alt="Hugo Profile Picture" width={0} height={0} sizes="100vw" style={{width: "100%", height: "auto"}}/>
-    </div>
+    </section>
   )
 }
 
