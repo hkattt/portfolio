6e4a504b771c49afeba053f4cf57579acd76cb77
hkattt <hugo.kat3@gmail.com>
2025-01-08 17:33:54 +1100

    Improved dark mode colours

--- app/app/components/Footer/Footer.tsx
+++ app/app/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 import styles from "./Footer.module.scss";
 
@@ -6,7 +7,10 @@ const Footer = () => {
   return (
     <footer className={styles.footer} id="Footer" aria-label="Footer">
         <div>
-            <p className={styles.message}>Designed with 💙 by Hugo Kat</p>
+            <p className={styles.message}>Designed in <Link href="https://www.figma.com/" target="_blank" rel="noopener noreferrer">Figma</Link> with 
+            the <Link href="https://www.nordtheme.com/" target="_blank" rel="noopener noreferrer">Nord Theme</Link>, built 
+            using <Link href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">Next.js</Link>, and
+            deployed by <Link href="https://vercel.com/" target="_blank" rel="noopener noreferrer">Vercel</Link>.</p> 
             <p className={styles.copyright}>© 2025 Hugo Kat. All rights reserved.</p>
         </div>
     </footer>
