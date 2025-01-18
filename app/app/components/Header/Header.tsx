9c33dda75e46a180421c78d36db537a7042d0785
hkattt <hugo.kat3@gmail.com>
2025-01-18 16:28:07 +1100

    Desktop navigation bar and revamped theme button

--- app/app/components/Header/Header.tsx
+++ app/app/components/Header/Header.tsx
@@ -1,19 +1,28 @@
+"use client"
 import React from "react";
 import Link from "next/link";
+
 import ThemeButton from "../ThemeButton/ThemeButton";
 
 import styles from "./Header.module.scss";
 
 const Header = () => {
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id);
+    element?.scrollIntoView({behavior: "smooth", block: "end"})
+  };
+
   return (
     <header className={styles.header} id="header" aria-label="Header">
         <div className={styles.content}>
-            <nav>
-              <Link href="#hero" rel="noopener noreferrer">Home</Link>
-              <Link href="#projects" rel="noopener noreferrer">Projects</Link>
-              <Link href="#skills" rel="noopener noreferrer">Skills</Link>
-              <Link href="#experiences" rel="noopener noreferrer">Experience</Link>
-            </nav>
+            <span className={styles.bar}>
+              <nav>
+                <Link href="/" rel="noopener noreferrer">Home</Link>
+                <Link href="/#projects" rel="noopener noreferrer">Projects</Link>
+                <Link href="/#skills" rel="noopener noreferrer">Skills</Link>
+                <Link href="/#experiences" rel="noopener noreferrer">Experience</Link>
+              </nav>
+            </span>
             <ThemeButton/>
         </div>
     </header>
