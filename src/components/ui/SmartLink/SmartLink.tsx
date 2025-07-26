7fe4352c5b06dfb4d80034d9f96dcb1bffd993fe
hkattt <hugo.kat3@gmail.com>
2025-07-26 13:29:17 +1000

    Created SmartLink component

---
+++ src/components/ui/SmartLink/SmartLink.tsx
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+import Link from 'next/link';
+
+import styles from './SmartLink.module.scss';
+
+type SmartLinkProps = {
+  href?: string;
+  children?: React.ReactNode;
+  target?: React.HTMLAttributeAnchorTarget;
+}
+
+export const SmartLink: React.FC<SmartLinkProps> = ({ href, children, target = '_self'}) => {
+  if (!href) {
+    return children;
+  }
+
+  return (
+    <Link className={styles.link} href={href} target={target} rel="noopener noreferrer">
+      {children}
+    </Link>
+  );
+};
