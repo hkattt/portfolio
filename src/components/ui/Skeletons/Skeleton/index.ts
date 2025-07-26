94619a263fb556da3f01a9c306d6ec4f28479966
hkattt <hugo.kat3@gmail.com>
2025-07-27 09:42:53 +1000

    Created Skeleton components

---
+++ src/components/ui/Skeletons/Skeleton/index.ts
@@ -0,0 +1,1 @@
+﻿export { Skeleton } from './Skeleton';
---
+++ src/components/ui/Skeletons/Skeletons.tsx
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+
+import { Skeleton } from './Skeleton';
+import type { Direction } from '@/lib/types';
+
+import styles from './Skeletons.module.scss';
+
+type SkeletonsProps = {
+  n: number;
+  className?: string;
+  width?: number | string;
+  height?: number | string;
+  direction?: Direction;
+}
+
+export const Skeletons: React.FC<SkeletonsProps> = ({ n, className, width, height, direction = 'vertical' }) => {
+  return (
+    <div
+      style={{ flexDirection: direction === 'vertical' ? 'column' : 'row' }}
+      className={styles.skeletons}
+    >
+      {
+        Array(n).fill(null).map((_, index) => (
+          <Skeleton key={index} className={className} width={width} height={height} />
+        ))
+      }
+    </div>
+  );
+};
