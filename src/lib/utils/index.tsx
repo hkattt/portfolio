dfd75d9a6c4e3c2711f1ae4c5d5ce1df44cde74d
hkattt <hugo.kat3@gmail.com>
2025-07-26 16:13:16 +1000

    Created record mapping functions

---
+++ src/lib/utils/index.tsx
@@ -0,0 +1,63 @@
+﻿import { FaGithub } from 'react-icons/fa';
+
+import { pocketbaseUrl } from '@/lib/config';
+import type { PortfolioItem, Technology } from '@/lib/types';
+import { RecordModel } from 'pocketbase';
+
+export const mapProjectToPortfolioItem = (projectRecord: RecordModel): PortfolioItem => {
+  return {
+    image: {
+      src: `${pocketbaseUrl}/api/files/projects/${projectRecord["id"]}/${projectRecord["image"]}`,
+      alt: projectRecord['alt'],
+      width: 0,
+      height: 0
+    },
+    title: projectRecord['title'],
+    titleLink: projectRecord['online'],
+    startDate: new Date(projectRecord['date']),
+    endDate: undefined,
+    description: projectRecord['description'],
+    tags: projectRecord['technologies'].map((technology: Technology) => ({
+      text: technology.name
+    })),
+    extraLinks: [
+      {
+        icon: <FaGithub />,
+        href: projectRecord["github"]
+      }
+    ],
+    onGoing: false,
+    sortOrder: projectRecord['sortOrder']
+  }
+}
+
+export const mapExperienceToPortfolioItem = (experienceRecord: RecordModel): PortfolioItem => {
+  console.log(experienceRecord);
+  return {
+    title: experienceRecord['title'],
+    subtitle: experienceRecord['organisation'].title,
+    subtitleLink: experienceRecord['organisation'].href,
+    startDate: new Date(experienceRecord['startDate']),
+    endDate: experienceRecord['endDate'] ? new Date(experienceRecord['endDate']) : undefined,
+    description: experienceRecord['description'],
+    tags: experienceRecord['technologies'].map((technology: Technology) => ({
+        text: technology.name
+    })),
+    extraLinks: [],
+    onGoing: true,
+    sortOrder: experienceRecord['sortOrder']
+  }
+}
+
+export const mapEducationToPortfolioItem = (educationRecord: RecordModel): PortfolioItem => {
+  return {
+    title: educationRecord['institution'],
+      subtitle: educationRecord['qualification'],
+    startDate: new Date(educationRecord['startDate']),
+    endDate: educationRecord['endDate'] ? new Date(educationRecord['endDate']) : undefined,
+    tags: [],
+    extraLinks: [],
+    onGoing: true,
+    sortOrder: educationRecord['sortOrder']
+  }
+}
