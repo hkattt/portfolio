efd655189eb9687ae3ede456b4d270c98533ebe9
hkattt <hugo.kat3@gmail.com>
2025-07-26 16:11:21 +1000

    Created Experiences component

---
+++ src/components/Experiences/Experiences.tsx
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+
+import { Section } from '@/components/ui/Section';
+import { PortfolioItems } from '@/components/ui/PortfolioItems';
+import type { PortfolioItem as PortfolioItemType } from '@/lib/types';
+
+type ExperiencesProps = {
+  experiences: PortfolioItemType[];
+}
+export const Experiences: React.FC<ExperiencesProps> = ({ experiences }) => {
+  experiences.sort((a, b) => a.sortOrder - b.sortOrder);
+
+  return (
+    <Section title='WORK EXPERIENCE'>
+      <PortfolioItems portfolioItems={experiences} />
+    </Section>
+  );
+};
