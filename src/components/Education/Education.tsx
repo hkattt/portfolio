efdbfd44b67371c744d25a28afc7776e0d87fcde
hkattt <hugo.kat3@gmail.com>
2025-07-26 16:12:19 +1000

    Created Education component

---
+++ src/components/Education/Education.tsx
@@ -0,0 +1,19 @@
+﻿import React from 'react';
+
+import { Section } from '@/components/ui/Section';
+import { PortfolioItems } from '@/components/ui/PortfolioItems';
+import type { PortfolioItem as PortfolioItemType } from '@/lib/types';
+
+type EducationProps = {
+  education: PortfolioItemType[];
+}
+
+export const Education: React.FC<EducationProps> = ({ education }) => {
+  education.sort((a, b) => a.sortOrder - b.sortOrder);
+
+  return (
+    <Section title='EDUCATION'>
+      <PortfolioItems portfolioItems={education} />
+    </Section>
+  );
+};
