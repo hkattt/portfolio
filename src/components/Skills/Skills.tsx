df88d9b5a5b4ededb2fd63d89e44e2406e77f845
hkattt <hugo.kat3@gmail.com>
2025-07-26 16:10:54 +1000

    Created Skills component

---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+
+import { Card } from '@/components/ui/Card/Card';
+import { Tags } from '@/components/ui/Tags';
+import { Section } from '../ui/Section';
+import type { SkillSet } from '@/lib/types';
+
+import styles from './Skills.module.scss';
+
+type SkillsProps = {
+  skillSets: SkillSet[];
+}
+
+export const Skills: React.FC<SkillsProps> = ({ skillSets }) => {
+  return (
+    <Section title='SKILLS'>
+      <div className={styles.skills}>
+        {
+          skillSets.map((skillSet, index) => (
+            <Card key={index} className={styles.skill} title={skillSet.title} icon={skillSet.icon}>
+              <Tags tags={skillSet.skills} />
+            </Card>
+          ))
+        }
+      </div>
+    </Section>
+  );
+};
---
+++ src/components/Skills/index.ts
@@ -0,0 +1,1 @@
+﻿export { Skills } from './Skills';
