aef3a7f969d8d753433d89940986799da345d5be
hkattt <hugo.kat3@gmail.com>
2025-07-26 13:30:26 +1000

    Created PortfolioItems component

---
+++ src/components/PortfolioItems/PortfolioItem/PortfolioItem.tsx
@@ -0,0 +1,50 @@
+﻿import React from 'react';
+
+import { Tag } from '@/components/ui/Tag';
+import { Date } from '@/components/ui/Date';
+import { LinkButton } from '@/components/ui/Button';
+import { SmartLink } from '@/components/ui/SmartLink';
+import type { PortfolioItem as PortfolioItemType } from '@/lib/types';
+
+import styles from './PortfolioItem.module.scss';
+
+type PortfolioItemProps = {
+  portfolioItem: PortfolioItemType;
+}
+
+export const PortfolioItem: React.FC<PortfolioItemProps> = ({ portfolioItem }) => {
+  return (
+    <div className={styles.item}>
+      <div className={styles.date}>
+        <Date startDate={portfolioItem.startDate} endDate={portfolioItem.endDate} onGoing={portfolioItem.onGoing} />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <SmartLink href={portfolioItem.titleLink} target='_blank'>
+            <h3 className={styles.title}>{portfolioItem.title}</h3>
+          </SmartLink>
+          { portfolioItem.extraLinks.length > 0 && (
+            <div>
+              {
+                portfolioItem.extraLinks.map((link, index) => (
+                  <LinkButton key={index} href={link.href} icon={link.icon} />
+                ))
+              }
+            </div>
+          )}
+        </div>
+        { portfolioItem.subtitle && <span className={styles.subtitle}>{portfolioItem.subtitle}</span> }
+        { portfolioItem.description && <p className={styles.description}>{portfolioItem.description}</p> }
+        { portfolioItem.tags.length > 0 && (
+          <div className={styles.tags}>
+            {
+              portfolioItem.tags.map((tag, index) => (
+                <Tag key={index} text={tag} />
+              ))
+            }
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/PortfolioItems/PortfolioItems.tsx
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+
+import { PortfolioItem } from './PortfolioItem';
+import { Divider } from '@/components/ui/Divider';
+import type { PortfolioItem as PortfolioItemType } from '@/lib/types';
+
+type PortfolioItemsProps = {
+  portfolioItems: PortfolioItemType[];
+}
+
+export const PortfolioItems: React.FC<PortfolioItemsProps> = ({ portfolioItems }) => {
+  return (
+    <div>
+      {
+        portfolioItems.map((portfolioItem, index) => (
+          <div key={index}>
+            <PortfolioItem portfolioItem={portfolioItem} />
+            { index < portfolioItems.length - 1 && <Divider /> }
+          </div>
+        ))
+      }
+    </div>
+  );
+};
