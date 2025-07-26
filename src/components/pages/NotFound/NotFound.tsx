b83f2c4a86156438edc57731470ad955e0ec2cf0
hkattt <hugo.kat3@gmail.com>
2025-07-26 17:07:58 +1000

    Created NotFound component

---
+++ src/components/pages/NotFound/NotFound.tsx
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+import { FaTimesCircle } from 'react-icons/fa';
+
+import { LinkButton } from '@/components/ui/Button';
+
+import styles from './NotFound.module.scss';
+
+export const NotFound: React.FC = () => {
+  return (
+    <div className={styles.notFound}>
+      <FaTimesCircle size={48} />
+      <h2 className={styles.errorCode}>ERROR 404</h2>
+      <h1 className={styles.title}>PAGE NOT FOUND</h1>
+      <LinkButton href='/' target='_self'>Take me home</LinkButton>
+    </div>
+  );
+};
---
+++ src/components/pages/NotFound/index.ts
@@ -0,0 +1,1 @@
+﻿export { NotFound } from './NotFound';
