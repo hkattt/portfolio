d5716e60f561f2f239d9b034fa4f73db173a62f4
hkattt <hugo.kat3@gmail.com>
2025-07-26 13:31:29 +1000

    Created collection service

---
+++ src/services/collectionService.ts
@@ -0,0 +1,21 @@
+﻿import useSWR from 'swr';
+import { RecordModel } from 'pocketbase';
+
+import { pb } from '@/lib/pocketbase';
+
+const fetchCollection = async (collectionName: string): Promise<RecordModel[]> => {
+  return await pb.collection(collectionName).getFullList();
+}
+
+export const useGetCollection = (collectionName: string) => {
+  const { data, isLoading, error } = useSWR<RecordModel[]>(
+    collectionName,
+    fetchCollection
+  );
+
+  return {
+    data,
+    error,
+    isLoading
+  }
+}
