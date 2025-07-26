a41f2ad20ec00ca3c3297cc67a5414b59a0bd589
hkattt <hugo.kat3@gmail.com>
2025-07-26 13:29:02 +1000

    Created Tag component

---
+++ src/components/ui/Tag/Tag.tsx
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+
+import styles from './Tag.module.scss';
+
+type TagProps = {
+  text: string;
+  icon?: React.ReactNode;
+}
+
+export const Tag: React.FC<TagProps> = ({ text, icon }) => {
+  return (
+    <div className={styles.tag}>
+      {icon}
+      <span className={styles.text}>{text}</span>
+    </div>
+  );
+};
