a2e36caa9be7aff928fd98094008a57d37444235
hkattt <hugo.kat3@gmail.com>
2025-07-27 09:41:59 +1000

    Removed Divider component

--- src/components/ui/Divider/Divider.tsx
+++
@@ -1,9 +0,0 @@
-﻿import React from 'react';
-
-import styles from './Divider.module.scss';
-
-export const Divider: React.FC = () => {
-  return (
-    <div className={styles.divider} />
-  );
-};
--- src/components/ui/Divider/index.ts
+++
@@ -1,1 +0,0 @@
-﻿export { Divider } from './Divider';
