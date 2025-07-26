e642a20bd38584d1b6624e9246ad581666a5bd2a
hkattt <hugo.kat3@gmail.com>
2025-07-26 13:30:40 +1000

    Created Section component

---
+++ src/components/Section/Section.tsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+
+import styles from './Section.module.scss';
+
+type SectionProps = {
+  title: string;
+  children: React.ReactNode;
+}
+
+export const Section: React.FC<SectionProps> = ({ title, children }) => {
+  return (
+    <section className={styles.section}>
+      <div className={styles.header}>
+        <div className={styles.bar} />
+        <h2 className={styles.title}>{title}</h2>
+      </div>
+      {children}
+    </section>
+  );
+};
