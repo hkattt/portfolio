3a07d76e901894b61263ad1ef914390033e59de2
hkattt <hugo.kat3@gmail.com>
2025-07-26 13:36:41 +1000

    Created Button component

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import Link from 'next/link';
+
+import styles from './Button.module.scss';
+
+type ButtonType = 'primary' | 'text' | 'link';
+
+type ButtonProps = {
+  type: ButtonType;
+  children?: React.ReactNode;
+  icon?: React.ReactNode;
+}
+export const Button: React.FC<ButtonProps> = ({ children, icon }) => {
+  return (
+    <button className={styles.button}>
+      {icon}
+      {children}
+    </button>
+  );
+};
---
+++ src/components/ui/Button/index.ts
@@ -0,0 +1,2 @@
+﻿export { Button } from './Button';
+export { LinkButton } from './LinkButton';
