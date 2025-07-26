955d1315f7ad5d0113c41e261d7826bd38834d8c
hkattt <hugo.kat3@gmail.com>
2025-07-26 16:12:40 +1000

    Created Card component

---
+++ src/components/ui/Card/Card.tsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+
+import styles from './Card.module.scss';
+
+type CardProps = {
+  className?: string;
+  title: string;
+  icon: React.ReactNode;
+  children: React.ReactNode;
+}
+export const Card: React.FC<CardProps> = ({ className, title, icon, children }) => {
+  return (
+    <div className={[styles.card, className].filter(Boolean).join(' ')}>
+      <div className={styles.header}>
+        {icon}
+        <h3 className={styles.title}>{title}</h3>
+      </div>
+      {children}
+    </div>
+  );
+};
