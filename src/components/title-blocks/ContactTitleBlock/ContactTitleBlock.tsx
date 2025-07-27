e4363b7816a4784f93180f4d112b9ba68e2e6bc7
hkattt <hugo.kat3@gmail.com>
2025-07-27 12:03:49 +1000

    Created ContactTitleBlock and Contact page

---
+++ src/components/title-blocks/ContactTitleBlock/ContactTitleBlock.tsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+import { TitleBlock } from '@/components/ui/TitleBlock';
+export const ContactTitleBlock: React.FC = () => {
+  return (
+    <TitleBlock title='Contact'>
+      <p>
+        Feel free to contact me at{' '}
+      </p>
+    </TitleBlock>
+  );
+};
