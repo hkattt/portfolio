0349337c4b87dc6a09e9673f21a3e3228b555a7f
hkattt <hugo.kat3@gmail.com>
2025-07-27 11:59:08 +1000

    Updating next remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'portfolio-pocketbase-preview.pockethost.io',
+        pathname: '/api/files/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'portfolio-pocketbase-prod.pockethost.io',
+        pathname: '/api/files/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
