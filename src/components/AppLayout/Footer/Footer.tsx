ac25f1ddbffdfd107e0e7d62d2651d2ee3dd09ce
hkattt <hugo.kat3@gmail.com>
2025-07-27 13:32:47 +1000

    Improved Footer LinkedIn logo

--- src/components/AppLayout/Footer/Footer.tsx
+++ src/components/AppLayout/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 ﻿import React from 'react';
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn, FaEnvelope } from 'react-icons/fa';
 
 import { LinkButton } from "@/components/ui/Button";
 import styles from "./Footer.module.scss";
@@ -12,7 +12,7 @@ export const Footer: React.FC = () => {
       <p>© 2025 Hugo Kat. All rights reserved.</p>
       <div>
         <LinkButton href={appConstants.socialUrls.github} icon={<FaGithub />} />
-        <LinkButton href={appConstants.socialUrls.linkedin} icon={<FaLinkedin />} />
+        <LinkButton href={appConstants.socialUrls.linkedin} icon={<FaLinkedinIn />} />
         <LinkButton href={`mailto:${appConstants.socialUrls.emailAddress}`} icon={<FaEnvelope />} />
       </div>
     </footer>
