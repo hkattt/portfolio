6d2b95152e8c68393a117cd4339125a84d1a7648
hkattt <hugo.kat3@gmail.com>
2025-07-27 13:48:48 +1000

    Created ResultPage component

---
+++ src/components/pages/ResultPage/ResultPage.tsx
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+
+import styles from './ResultPage.module.scss';
+
+type ResultPageProps = {
+  title: string;
+  subtitle?: string;
+  icon?: React.ReactNode;
+  children?: React.ReactNode;
+}
+export const ResultPage: React.FC<ResultPageProps> = ({ title, subtitle, icon, children }) => {
+  return (
+    <div className={styles.result}>
+      {icon}
+      { subtitle && <h2 className={styles.subtitle}>{subtitle}</h2> }
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.content}>
+        {children}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/pages/ResultPage/index.ts
@@ -0,0 +1,1 @@
+﻿export { ResultPage } from './ResultPage';
