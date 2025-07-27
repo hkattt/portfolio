df0609695f80106a60ae8323a7553bb00a2288ff
hkattt <hugo.kat3@gmail.com>
2025-07-27 15:31:53 +1000

    Removed loading page and component

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-﻿import React from 'react';
-
-import { Loading as LoadingComponent } from '@/components/pages/Loading';
-
-const Loading: React.FC = () => {
-  return <LoadingComponent />;
-};
-
-export default Loading;
--- src/components/pages/Loading/index.ts
+++
@@ -1,1 +0,0 @@
-﻿export { Loading } from './Loading';
