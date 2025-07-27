a8f496cb192a40ac0dd6afd43124707893261e22
hkattt <hugo.kat3@gmail.com>
2025-07-27 11:52:08 +1000

    Created single record fetch hook

--- src/services/collectionService.ts
+++ src/services/collectionService.ts
@@ -7,10 +7,27 @@ const fetchCollection = async (collectionName: string): Promise<RecordModel[]> =
   return await pb.collection(collectionName).getFullList();
 }
 
+const fetchSingleRecord = async (collectionName: string): Promise<RecordModel> => {
+  return await pb.collection(collectionName).getFirstListItem('');
+}
+
 export const useGetCollection = (collectionName: string) => {
   const { data, isLoading, error } = useSWR<RecordModel[]>(
     collectionName,
     fetchCollection
+  );
+
+  return {
+    data,
+    error,
+    isLoading
+  }
+}
+
+export const useGetSingleRecord = (collectionName: string) => {
+  const { data, isLoading, error } = useSWR<RecordModel>(
+    collectionName,
+    fetchSingleRecord
   );
 
   return {
