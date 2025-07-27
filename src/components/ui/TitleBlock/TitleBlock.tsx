1623c1adcf8555bd52982f1accf1a7cbd8f4aed3
hkattt <hugo.kat3@gmail.com>
2025-07-27 11:51:01 +1000

    Created TitleBlock component

---
+++ src/components/ui/TitleBlock/TitleBlock.tsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+
+import styles from './TitleBlock.module.scss';
+
+type TitleBlockProps = {
+  title: string;
+  subtitle?: string;
+  children?: React.ReactNode;
+}
+
+export const TitleBlock: React.FC<TitleBlockProps> = ({ title, subtitle, children }) => {
+  return (
+    <div className={styles.titleBlock}>
+      <div className={styles.header}>
+        <h1>{title}</h1>
+        {subtitle && <h2>{subtitle}</h2>}
+      </div>
+      {children && <div className={styles.content}>{children}</div> }
+    </div>
+  );
+};
